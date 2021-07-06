57a819b2ea3e6c80d929aaf5d9e01748ca67a748
Axel Stankiewicz <axelxbox126@gmail.com>
2021-07-06 15:43:22 +0200

    Fix Header + Ajouts boutons + Fix CSS

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -5,7 +5,7 @@ window.onscroll = function () {
         document.getElementById("navbar").style.padding = "0px";
     }
     else{
-        document.getElementById("navbar").style.background = "#262285";
+        document.getElementById("navbar").style.background = "none";
         document.getElementById("navbar").style.height = "80px";
         document.getElementById("navbar").style.padding = "0px";
     }
